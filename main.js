15312dc74a4b29464aecbba11984a5cbef2ba037
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-03 16:14:07 +0200

    fixing design and responsiveness

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ response
         console.log(element);
     });
     const wonder_list=data.map(element=>({
-        name:element.name,
+    name:element.name,
     build_year:element.build_year,
     categories:element.categories,
     links:element.links,
@@ -19,81 +19,41 @@ response
     time_period:element.time_period,
 
     }),
-    
 );
 //wonder_name=wonder_list[0].name;
-
 const container = document.getElementById('container');
 document.body.appendChild(container);
 wonder_list.forEach(element=>{
 
 
-let box = document.createElement('box');
-const img = document.createElement('img');
-img.src = element.links.images[0]; 
-img.alt = 'Placeholder Image';
-img.style.Width = '500px';
-img.style.height = '200px';
-img.style.borderRadius = '5px';
-img.style.margin = '5px';
+    let box = document.createElement('box');
+    const img = document.createElement('img');
+    img.src = element.links.images[0]; 
+    img.style.width = '100%';
+    img.style.height = '200px';
+    img.style.borderRadius = '0 px';
+    img.style.margin = '5 px';
 
 
-let name = document.createElement('p');
-name.textContent = element.name; 
-name.style.margin = '10px 0 0';
-name.style.fontSize = '18px';
-name.style.color = '#072e37';
-name.style.backgroundColor='beige'
+    let name = document.createElement('p');
+    name.textContent = element.name; 
+    name.style.margin = '10px 0 0';
+    name.style.fontSize = '18px';
+    name.style.color = '#072e37';
+    name.style.backgroundColor='beige'
 
 
-box.appendChild(img);
-box.appendChild(name);
+    box.appendChild(img);
+    box.appendChild(name);
 
 
-container.appendChild(box);
-box.addEventListener('click',()=>{
-        localStorage.setItem('selected',JSON.stringify(element));
-        window.location.href = "data.html"; })
+    container.appendChild(box);
+    box.addEventListener('click',()=>{
+            localStorage.setItem('selected',JSON.stringify(element));
+            window.location.href = "data.html"; })
     
-})
-  /*container =document.getElementById('on-col');
-  let img = document.createElement('img');
-        img.src = element.links.images[0]; 
-        img.style.width = '300px';
-        img.style.height = 'auto';
-       
-    let li = document.createElement('name');
-    li.style.color='beige';
-    li.style.padding = '20px';
-    li.style.border='2px solid beige'  
-    li.style.borderRadius = '10px'
-    li.style.margin = '10px'; 
-    //li.style.boxShadow = '0 4px 10px rgba(0, 0, 0, 0.2)';
-    console.log(element.name);
-    li.textContent = element.name;
-    
-    counter++;
-    /*if(counter%3  ===0){
-        const br=document.createElement('li');
-        br.textContent="";
-        container.appendChild(br);
-    }
-    container.appendChild(img);
-    container.appendChild(li);
-    
-    
-        
-/*let col=document.createElement('on-col');
-    col.appendChild(container);*/
-
-       /*li.addEventListener('click',()=>{
-        localStorage.setItem('selected',JSON.stringify(element));
-        window.location.href = "data.html"; 
-    })*/
-
-
-
-
+    })
+  
 })
   .catch((error) => {
     console.log(error);
--- wonder-data.js
+++ wonder-data.js
@@ -1,13 +1,28 @@
 const selected = JSON.parse(localStorage.getItem('selected'));
-//document.body.style.backgroundColor='beige';
 if(selected){
+    
     document.getElementById('name').textContent = selected.name;
-    document.getElementById('year').textContent = selected.build_year;
-    document.getElementById('categories').textContent = selected.categories;
-    document.getElementById('links').textContent = selected.links;    
-    document.getElementById('summary').textContent = selected.summary;
-    document.getElementById('location').textContent = selected.location;
-    //document.getElementById('time-period').textContent = selected.time_period;
+    document.getElementById('year').textContent ='Year of Origin: '+selected.build_year;
+    document.getElementById('categories').textContent ='Categories: '+ selected.categories;
+    let wiki=document.getElementById('links-wiki');
+    wiki.textContent = '🇼 for more Info';
+    wiki.style.cursor = 'pointer'; 
+    wiki.addEventListener('click', () => {
+        window.open(selected.links.wiki, "");  
+    });
+    wiki.style.justifyContent='flex-end';
+    ///selected.links.wiki;    
+    let maps=document.getElementById('links-maps')
+    maps.textContent = '🌎 Check on Google Maps';
+    ///selected.links.google_maps; 
+    maps.style.cursor = 'pointer'; 
+    maps.addEventListener('click', () => {
+        window.open(selected.links.google_maps, "");  
+    });
+    maps.style.justifyContent='flex-end';
+    document.getElementById('summary').textContent ='Summary: '+selected.summary;
+    document.getElementById('location').textContent ='Location: ' +selected.location;
+    document.getElementById('time-period').textContent = 'Time Period: '+selected.time_period;
     
     cont=document.getElementById('cont');
         let img = document.createElement('img');
@@ -15,9 +30,10 @@ if(selected){
         container.innerHTML = '';
         img.src = selected.links.images[1]; 
         img.style.width = '100%';
+        //img.style.minWidth='250px'
         img.style.height = '300px';
         
-        img.style.margin='10px';
+        img.style.margin='2px';
         
         container.appendChild(img);
         cont.appendChild(container);
@@ -26,16 +42,12 @@ if(selected){
         container2.innerHTML = '';
         img2.src = selected.links.images[2]; 
         img2.style.width = '100%';
+        //img2.style.minWidth='250px'
         img2.style.height = '300px';
         
-        img2.style.margin='10px';
+        img2.style.margin='2px';
         container2.appendChild(img2);
         cont.appendChild(container2);
-
-
-        
-
-    
 } 
 
 
